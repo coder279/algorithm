78e9724ebfa1e072ad8039245b7503d3e15fffb0
coder279 <coder279@126.com>
2024-04-08 07:59:42 -0400

    feat 数据结构--链表

---
+++ linkedList/build/CMakeFiles/linkedList.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linkedList.dir/src/main.cc.o"
+  "linkedList"
+  "linkedList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ linkedList/src/main.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(nullptr){}
+};
+
+/** 
+ * 链表插入数据
+ *
+ * @param n0
+ * @param p 
+ */
+void insert(ListNode *n0, ListNode *p){
+  ListNode *n1 = n0->next;
+  p->next = n1;
+  n0->next = p;
+}
+
+/** 
+ * 链表删除数据
+ * 
+ * @param n0
+ */
+void remove(ListNode *n0){
+  if(n0->next == nullptr){
+    return;
+  }
+  ListNode *p = n0->next;
+  ListNode *n1 = p->next;
+  n0->next = n1;
+  delete p;
+}
+
+/** 
+ * 遍历链表
+ * 
+ * @param head 
+ * @param index
+ * 
+ * @return 
+ */
+ListNode *access(ListNode *head, int index){
+  for(int i = 0; i < index; i++){
+    if(head == nullptr){
+      return nullptr;
+    }
+    head = head->next;
+  }
+  return head;
+}
+
+int find(ListNode *head, int target){
+  int index = 0;
+  while (head != nullptr){
+    if (head->val == target){
+      return index;
+    }
+    head = head->next;
+  }
+  return index;
+}
+int main(){
+  ListNode *n0 = new ListNode(1);
+  ListNode *n1 = new ListNode(2);
+  ListNode *n2 = new ListNode(3);
+  ListNode *n3 = new ListNode(4);
+  ListNode *n4 = new ListNode(5);
+
+  // 构造节点
+  n0->next = n1;
+  n1->next = n2;
+  n2->next = n3;
+  n3->next = n4;
+  insert(n1, n3);
+  return 0;
+}
